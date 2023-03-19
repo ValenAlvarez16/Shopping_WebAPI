0867ae5711a723558281aa5e4e23473ea3690451
VALENTINA ALVAREZ PANIAGUA <valentinaalvarez305940@correo.itm.edu.co>
2023-03-19 18:51:25 -0500

    New WebAPI Shopping project with new Country Entity

---
+++ Shopping_WebAPI1/Shopping_WebAPI1/Entities/Country.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Shopping_WebAPI1.Entities
+{
+    public class Country: Entity
+    {
+        [Display(Name = "País")]//mostrar el name con otro nombre
+        [MaxLength(50,ErrorMessage = "El campo {0} debe tener máximo {1} caracteres.")]//LONGITUD MÁXIMA (NVARCHAR(50))
+        [Required(ErrorMessage ="El campo {0} es obligatorio.")]//NOT NULL
+        public string Name { get; set; }
+    }
+}
---
+++ Shopping_WebAPI1/Shopping_WebAPI1/Entities/Entity.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Shopping_WebAPI1.Entities
+{
+    public class Entity
+    {
+        [Key] //Decoradores
+
+        public Guid Id { get; set; }
+
+        public DateTime? CreatedDate { get; set; }//? indica que puede ser null
+        public DateTime? ModifiedDate { get; set; }
+    }
+}
