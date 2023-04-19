05901b411e834f285f032298ded81c9ef4162307
VALENTINA ALVAREZ PANIAGUA <valentinaalvarez305940@correo.itm.edu.co>
2023-04-18 20:47:58 -0500

    New class Countries Controller

---
+++ Shopping_WebAPI1/Shopping_WebAPI1/Controllers/CountriesController.cs
@@ -0,0 +1,110 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Shopping_WebAPI1.DAL;
+using Shopping_WebAPI1.Entities;
+using DbUpdateException = Microsoft.EntityFrameworkCore.DbUpdateException;
+
+namespace Shopping_WebAPI1.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CountriesController : ControllerBase
+    {
+        private readonly DataBaseContext _context;
+
+        public CountriesController(DataBaseContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet, ActionName("Get")]
+        [Route("Get")]
+        public async Task<ActionResult<IEnumerable<Country>>> GetCountries()
+        {
+            var countries = await _context.Countries.ToListAsync(); // Select * From Countries
+
+            if (countries == null) return NotFound();
+
+            return countries;
+        }
+
+        [HttpGet, ActionName("Get")]
+        [Route("Get/{id}")]
+        public async Task<ActionResult<Country>> GetCountryById(Guid? id)
+        {
+            var country = await _context.Countries.FirstOrDefaultAsync(c => c.Id == id); //Select * From Countries Where Id = "..."
+
+            if (country == null) return NotFound();
+
+            return Ok(country);
+        }
+
+        [HttpPost, ActionName("Create")]
+        [Route("Create")]
+        public async Task<ActionResult> CreateCountry(Country country)
+        {
+            try
+            {
+                country.Id = Guid.NewGuid();
+                country.CreatedDate = DateTime.Now;
+
+                _context.Countries.Add(country);
+                await _context.SaveChangesAsync(); // Aquí es donde se hace el Insert Into...
+            }
+            catch (DbUpdateException dbUpdateException)
+            {
+                if (dbUpdateException.InnerException.Message.Contains("duplicate"))
+                    return Conflict(String.Format("{0} ya existe", country.Name));
+            }
+            catch (Exception ex)
+            {
+                return Conflict(ex.Message);
+            }
+
+            return Ok(country);
+        }
+
+        [HttpPut, ActionName("Edit")]
+        [Route("Edit/{id}")]
+        public async Task<ActionResult> EditCountry(Guid? id, Country country)
+        {
+            try
+            {
+                if (id != country.Id) return NotFound("Country not found");
+
+                country.ModifiedDate = DateTime.Now;
+
+                _context.Countries.Update(country);
+                await _context.SaveChangesAsync(); // Aquí es donde se hace el Update...
+            }
+            catch (DbUpdateException dbUpdateException)
+            {
+                if (dbUpdateException.InnerException.Message.Contains("duplicate"))
+                    return Conflict(String.Format("{0} ya existe", country.Name));
+            }
+            catch (Exception ex)
+            {
+                return Conflict(ex.Message);
+            }
+
+            return Ok(country);
+        }
+
+        [HttpDelete, ActionName("Delete")]
+        [Route("Delete/{id}")]
+        public async Task<ActionResult> DeleteCountry(Guid? id)
+        {
+            if (_context.Countries == null) return Problem("Entity set 'DataBaseContext.Countries' is null.");
+            var country = await _context.Countries.FirstOrDefaultAsync(c => c.Id == id);
+
+            if (country == null) return NotFound("Country not found");
+
+            _context.Countries.Remove(country);
+            await _context.SaveChangesAsync(); //Este las veces del delete en sql
+
+            return Ok(String.Format("El país {0} fue eliminado!", country.Name));
+        }
+    }
+}
+
+
